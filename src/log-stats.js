82da73c6f2d708a1284f2cf210f59b31a1272aac
king-king <niuyiwangqun@sohu.com>
2018-01-14 15:26:44 +0800

    change

--- src/log-stats.js
+++ src/log-stats.js
@@ -1,3 +1,5 @@
+/*eslint no-console: ["error", { allow: ["log"] }] */
+
 const stats = require('../lib/log').stats;
 const fs = require('fs');
 
@@ -5,12 +7,13 @@ stats({
     cwd: 'C:/Users/wangqun6/Downloads/TEST',
     args: {
         author: 'wangqun6',
-        after: '2017-12-03',
+        after: '2017-11-01',
         before: '2017-12-31'
     }
 }).then((content) => {
     // 成功
-    fs.writeFileSync('./out.json', JSON.stringify(content), 'utf-8');
+    fs.writeFileSync('./out.json', JSON.stringify(content.obj), 'utf-8');
+    fs.writeFileSync('./out.txt', content.raw, 'utf-8');
 }, (err) => {
     // 失败
     console.log(err);
